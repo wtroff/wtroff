45871029853ac24a7e26db369e1831e8393fc0d4
Shapturov Iulian <shapturov@intervolga.ru>
2018-03-08 21:00:06 +0300

    еще бы знать как миновать пробел (нажатие на кнопку)

--- Project/tepping-test/js/script.js
+++ Project/tepping-test/js/script.js
@@ -35,6 +35,9 @@ $(document).ready(function () {
             });
             $('html').keyup(function (event) {
                 if (event.keyCode == 32){
+                    $('.click-me').each(function (key,value) {
+                        $(value).attr('data-target','false');
+                    })
                     count++;
                     countInTab++;
                     CounterTimer();
@@ -67,6 +70,17 @@ $(document).ready(function () {
                             sourceKey[index] = countInTab;
                             index++;
                             console.info('keys',sourceKey);
+                            if(countInTab < 13){
+                                $('#warning-timer').text('Не теряй темп! Ты сможешь ^_^ ');
+                            }else if(countInTab > 55){
+                                $('#warning-timer').text('Очень высокий темп Waw ');
+                            } else if(countInTab > 40){
+                                $('#warning-timer').text('Превосходный темп :D ');
+                            } else if (countInTab > 30) {
+                                $('#warning-timer').text('Неплохой средний темп!');
+                            } else {
+                                $('#warning-timer').text('Лучше прибавить скорости');
+                            }
                             $('.add-count').append('<li class="list-group-item list-group-item-primary">'+ countInTab +'</li>');
                             countInTab = 0;
                         }, i*1000);
@@ -75,74 +89,127 @@ $(document).ready(function () {
                 }
 
             }
-            resultSum = 0;
+
+
             $('#result-alg').click(function () {
                 $('body').load('result.html .include-result-test',function () {
-
+                    var Equal = 0, resultSum = 0,
+                    averageFiveSec = 0, avarageOneSec = 0, sumAllPoints = 0;
                     for(var beg = 0; beg < sourceKey.length; beg++){
                         resultSum+= (sourceKey[beg] - sourceKey[0]);
-                    }
-                    var plus = 1.2;
-                    var Equal = Math.abs(resultSum);
-                    for(var num = 0, sizeTab = 0; num < 26; ++num, sizeTab+=plus){
-                        if((sizeTab - plus <= Equal)&&(sizeTab > Equal)) {
-                            Equal = Math.floor(num/5);
+                        sumAllPoints+= sourceKey[beg];
 
-                        }
-                        if(num == 5) plus = 1.8;
-                        if(num == 10) plus = 2.2;
-                        if(num == 15) plus = 2.8;
-                        if(num == 20) plus = 4;
                     }
-                    console.info('Eqal',Equal);
-                    console.info('reselt',resultSum);
-                    if(resultSum < 0) Equal*= (-1);
+                    if((Equal == 6)&&(resultSum > 0)) {
+                        Equal = 0;
+                    }
+                    averageFiveSec = parseInt(sumAllPoints/6);
+                    avarageOneSec = parseInt(sumAllPoints/30);
+
+                    if(Equal < 6) {
+                        var plus = 1.2;
+                        Equal = Math.abs(resultSum);
+                        for (var num = 0, sizeTab = 0; num < 26; ++num, sizeTab += plus) {
+                            if ((sizeTab - plus <= Equal) && (sizeTab > Equal)) {
+                                Equal = Math.floor(num / 5);
+                            }
+                            if (num == 5) plus = 1.8;
+                            if (num == 10) plus = 2.2;
+                            if (num == 15) plus = 2.8;
+                            if (num == 20) plus = 4;
+                        }
+                        console.info('Eqal', Equal);
+                        console.info('reselt', resultSum);
+                        if (resultSum < 0) {
+                            Equal *= (-1);
+                            if ((Equal == resultSum)) Equal = -5;
+                        }
+                        if ((Equal == resultSum)) Equal = 5;
+                    }
                     switch (Equal){
                         case 0:
                                 $('.show-result').empty();
-                                $('.show-result').append("Средняя нервная система</br>Ваш КСНС " + resultSum  );
+                                $('.show-result').append("Средняя нервная система</br>Ваш КСНС " + resultSum );
+                                $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                                $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                                 break;
                         case 1:
                                 $('.show-result').empty();
-                            $('.show-result').append("Небольшая выраженность силы нервной системы</br>Ваш КСНС " + resultSum );
+                                $('.show-result').append("Небольшая выраженность силы нервной системы</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case 2:
                             $('.show-result').empty();
                             $('.show-result').append("Средне-сильная нервная система</br>Ваш КСНС " + resultSum  );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case 3:
                             $('.show-result').empty();
                             $('.show-result').append("Сильная нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case 4:
                             $('.show-result').empty();
                             $('.show-result').append("Сильная нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп состavarageOneSecавляет" + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case 5:
                             $('.show-result').empty();
                             $('.show-result').append("Очень сильная нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case -1:
                             $('.show-result').empty();
                             $('.show-result').append("Небольшая выраженность слабости нервной системы</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case -2:
                             $('.show-result').empty();
                             $('.show-result').append("Средне-слабая нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case -3:
                             $('.show-result').empty();
                             $('.show-result').append("Слабая нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case -4:
                             $('.show-result').empty();
                             $('.show-result').append("Очень слабая нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
                         case -5:
                             $('.show-result').empty();
                             $('.show-result').append("Очень слабая нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
+                            break;
+                        case 6:
+                            $('.show-result').empty();
+                            $('.show-result').append("Вы слишком рано сдались: </br>Очень слабая нервная система</br>Ваш КСНС " + resultSum );
+                            $('.show-result').append("</br>Ваш средний темп составляет  " + averageFiveSec + "  кликов за пять секунд");
+                            $('.show-result').append("</br>Или " + avarageOneSec + "  кликов в секунду");
                             break;
 
+                    }
+                   if(averageFiveSec > 59) {
+
+                       $('.show-result').append("</br>Очень быстрый темп. Высокая подвижность нервной системы");
+                   }else if (averageFiveSec > 44) {
+                       $('.show-result').append("</br>Быстрый темп. подвижная нервная система");
+                   } else if (averageFiveSec > 31) {
+                       $('.show-result').append("</br>Средней подвижности нервная система");
+                   } else {
+                            $('.show-result').append("</br>Малоподвижная нервная система");
                     }
 
 
